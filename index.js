b22e061200b252bd17b0858a8c995c78e3400b62
Danielle Long <long.danielle.nicole@gmail.com>
2022-10-19 23:04:37 -0700

    commit tree 60b0ae2640aa7f3f8b2af19f72f714db486d62b5

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(sting){
+    console.log(sting.toUpperCase());
+}
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoomate(string){
+    if(string == string.toLowerCase()){
+    return "I can\'t hear you!";
+    } else if(string ==string.toUpperCase()){
+        return "YES INDEED!";
+    } else if(string == "Let\'s have dinner together!"){
+        return "I would love to!";
+    }}
